f70b6d0bf76a51ffcf8e35e2b3bf0126dffc8020
Thordreck <paezguerraalvaro@gmail.com>
2020-02-17 10:58:59 +0100

    Enable Qt resources in GUI by default.

--- gui/gui_main.cpp
+++ gui/gui_main.cpp
@@ -19,8 +19,9 @@ int main(int argc, char** argv)
     chip8::QInterpreter chip8_interpreter;
     qml_engine.rootContext()->setContextProperty("chip8Interpreter", &chip8_interpreter);
 
-    // TODO: qml_engine.load(QUrl("../gui/qml/main.qml"));
-    qml_engine.load(QUrl(qgetenv("MAIN_QML")));
+    // TODO:
+    qml_engine.load(QUrl("../gui/qml/main.qml"));
+    // qml_engine.load(QUrl(qgetenv("MAIN_QML")));
     // TODO: Enable this for release build
     //qml_engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
